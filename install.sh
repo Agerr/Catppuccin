95317924a27747f9b706e12ccb83047f7d5f3999
Agerr <agerrrr@proton.me>
2024-01-27 16:51:15 +0200

    a

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/shell
 mkdir -p ~/.config/i3
-ln -sf ~/Source/git/Catppuccin/files/.config/i3/config ~/.config/i3/config
+ln -sf ~/Source/git/Catppuccin/files/i3/config ~/.config/i3/config
 ln -sf ~/Source/git/Catppuccin/files/.Xresources ~/.Xresources
 ln -s ~/Source/git/Catppuccin/files/Backgrounds/* ~/Pictures/Backgrounds/
 ln -s ~/Source/git/Catppuccin/files/Backgrounds/arch-black-4k.png ~/Pictures/background.png
@@ -9,13 +9,13 @@ sudo cp ~/Source/git/Catppuccin/files/Backgrounds/arch-black-4k.png /usr/share/b
 sudo cp ~/Source/git/Catppuccin/files/picom.conf /etc/xdg/picom.conf
 sudo cp ~/Source/git/Catppuccin/files/lightdm.conf /etc/lightdm/lightdm.conf
 sudo cp ~/Source/git/Catppuccin/files/slick-greeter.conf /etc/lightdm/slick-greeter.conf
-ln -sf ~/Source/git/Catppuccin/files/.config/alacritty/* ~/.config/alacritty/
+ln -sf ~/Source/git/Catppuccin/files/alacritty/* ~/.config/alacritty/
 git clone https://github.com/catppuccin/alacritty.git ~/.config/alacritty/catppuccin/
 ln -sf ~/Source/git/Catppuccin/files/.zshrc ~/.zshrc
 curl -o ~/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh https://raw.githubusercontent.com/catppuccin/zsh-syntax-highlighting/main/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh
 #cp ~/Source/git/dotfiles-and-install-stuff/Dracula-zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/
 #cp -r ~/Source/git/dotfiles-and-install-stuff/Dracula-zsh/lib/ ~/.oh-my-zsh/themes/
-ln -sf ~/Source/git/Catppuccin/files/.config/dunstrc ~/.config/dunst/dunstrc
+ln -sf ~/Source/git/Catppuccin/files/dunstrc ~/.config/dunst/dunstrc
 
 ln -sf ~/Source/git/dotfiles-and-install-stuff/.config/cava ~/.config/cava
 mkdir -p ~/.config/BetterDiscord/themes
