b06a810694e1a595cf1d7e1ff10fe3db1c88b2b0
Agerr <agerrrr@proton.me>
2024-01-27 16:54:46 +0200

    g

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/shell
 mkdir -p ~/.config/i3
-ln -sf ~/Source/git/Catppuccin/files/i3/config ~/.config/i3/config
+ln -sf ~/Source/git/Catppuccin/files/i3 ~/.config/i3/config
 ln -sf ~/Source/git/Catppuccin/files/.Xresources ~/.Xresources
 ln -s ~/Source/git/Catppuccin/files/Backgrounds/* ~/Pictures/Backgrounds/
 ln -s ~/Source/git/Catppuccin/files/Backgrounds/arch-black-4k.png ~/Pictures/background.png
@@ -16,8 +16,8 @@ curl -o ~/.zsh/catppuccin_mocha-zsh-syntax-highlighting.zsh https://raw.githubus
 #cp ~/Source/git/dotfiles-and-install-stuff/Dracula-zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/
 #cp -r ~/Source/git/dotfiles-and-install-stuff/Dracula-zsh/lib/ ~/.oh-my-zsh/themes/
 ln -sf ~/Source/git/Catppuccin/files/dunstrc ~/.config/dunst/dunstrc
+ln -sf ~/Source/git/Catppuccin/files/cava ~/.config/cava/config
 
-ln -sf ~/Source/git/dotfiles-and-install-stuff/.config/cava ~/.config/cava
 mkdir -p ~/.config/BetterDiscord/themes
 ln -sf ~/Source/git/dotfiles-and-install-stuff/.config/BetterDiscord/themes/dracula.theme.css ~/.config/BetterDiscord/themes/dracula.theme.css
 sudo cp -r ~/Source/git/dotfiles-and-install-stuff/Dracula-GTK /usr/share/themes/Dracula
